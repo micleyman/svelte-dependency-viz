e663ddb3906b4f4812d0714c1b2578f0d5cf9754
Michiel Leyman <michiel.leyman@gmail.com>
2021-10-21 17:29:23 +0200

    build: make npm installable

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'fs';
+import path from 'path';
 
+const __dirname = path.resolve();
+const pkg = JSON.parse(readFileSync('./package.json'));
+
+console.log(pkg);
 const isProduction = process.env.NODE_ENV === 'production';
 export default defineConfig({
   plugins: [
@@ -9,5 +15,12 @@ export default defineConfig({
       emitCss: true,
       extensions: ['.svelte']
     })
-  ]
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: pkg.name,
+      fileName: (format) => `${pkg.name}.${format}.js`
+    }
+  }
 });
