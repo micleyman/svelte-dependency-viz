f8d5cccbdfeffa0e215abbf659c029d070ed930c
Michiel Leyman <michiel.leyman@gmail.com>
2021-10-21 17:56:38 +0200

    build: migrate to svelte-kit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import edges from '../../examples/edges.json';
+  import nodes from '../../examples/nodes.json';
+
+  import Graph from '$lib/Graph.svelte';
+</script>
+
+<Graph {nodes} {edges} />
